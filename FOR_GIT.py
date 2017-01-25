0b578874bf3b295b07810e6609f4ff65fdb3eca4
Sigfus Mar <sigfusvi227@nemi.tskoli.is>
2017-01-25 13:50:58 +0000

    Dæmi 1 tilbúið

--- FOR_GIT.py
+++ FOR_GIT.py
@@ -1,1 +1,15 @@
 print("Sigfús Már Viðarsson   FOR1TÖ05BU   Git Verkefni 25.1")
+print("")
+print("")
+print("Dæmi 1")
+print("Sláðu inn tvær tölur sem verða lagðar saman og svo margfaldaðar saman")
+tala1 = int(input("Skrifaðu inn tölu eitt: "))
+tala2 = int(input("Sláðu núna inn tölu tvö: "))
+summan = (tala1)+(tala2)
+margfoldun = tala1*tala2
+print("")
+print("Niðurstöðurnar verða þessar tölur...")
+print("Summan er: " , (summan))
+print("Margfölduninn verður: " , (margfoldun) )
+
+print("Dæmi 1 Lokið")
